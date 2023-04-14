c2d1786c7ba10de5871a8adb32bbd8fdc786635f
RajAnubhav <anubhavraj742@gmail.com>
2023-04-14 12:23:53 +0530

    t2

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
  echo "BUILD START"
  python3.9 -m pip install --upgrade pip 
- python3.9 -m pip install --upgrade setuptools
- python3.9 -m pip install neuralpy
+ python3.9 -m pip install --upgrade setuptools --user python
+ python3.9 -m pip install neuralpy --user python
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
