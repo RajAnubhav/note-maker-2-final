a1f9981d906e5edc6cbefd3e1f52e62b9a7c49c0
RajAnubhav <anubhavraj742@gmail.com>
2023-04-14 12:21:21 +0530

    t1

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,7 @@
  echo "BUILD START"
  python3.9 -m pip install --upgrade pip 
- python3.9 -m pip install virtualenv
- virtualenv env
- cd env/Scripts
- activate.bat
  python3.9 -m pip install --upgrade setuptools
+ python3.9 -m pip install neuralpy
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
