b02717bc6bc979316336141089adba4867ecf33d
RajAnubhav <anubhavraj742@gmail.com>
2023-04-14 11:27:42 +0530

    changed staticfiles_build to staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
