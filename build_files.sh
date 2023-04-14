7ae5c5d66d2efeccbdca53806ce5de5b9fdbaebe
RajAnubhav <anubhavraj742@gmail.com>
2023-04-14 11:16:05 +0530

    try

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
  echo "BUILD START"
  python3.9 -m pip install --upgrade pip 
+ python3.9 -m pip install virtualenv
+ virtualenv env
+ env\Scripts\activate
  python3.9 -m pip install --upgrade setuptools
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
