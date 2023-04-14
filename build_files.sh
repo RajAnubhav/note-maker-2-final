46382c62a8af728888069e7c2fbc8c907bc8a487
RajAnubhav <anubhavraj742@gmail.com>
2023-04-14 12:16:07 +0530

    virtualenv activate command

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,8 @@
  python3.9 -m pip install --upgrade pip 
  python3.9 -m pip install virtualenv
  virtualenv env
- "env\Scripts\activate"
+ cd env/Scripts
+ activate
  python3.9 -m pip install --upgrade setuptools
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
